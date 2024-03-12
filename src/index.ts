ea57c2cfef24d9c6437093c16e2d387aac9ab948
Emeryck <emeryck.clerin@outlook.com>
2024-03-12 15:16:24 +0100

    try example

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
-import type { ChartNode, PluginNodeDefinition, RivetPluginInitializer } from "@ironclad/rivet-core";
-// import {
-//   // examplePluginNode,
-//   // examplePluginNode2
-//   examplePluginNode3
-// } from "./nodes";
+// It is important that you only import types from @ironclad/rivet-core, and not
+// any of the actual Rivet code. Rivet is passed into the initializer function as
+// a parameter, and you can use it to access any Rivet functionality you need.
+import type { RivetPlugin, RivetPluginInitializer } from "@ironclad/rivet-core";
 
-import { examplePluginNode3 } from "./nodes/nodeExample3";
+import { examplePluginNode } from "./nodes/ExamplePluginNode";
 
+// A Rivet plugin must default export a plugin initializer function. This takes in the Rivet library as its
+// only parameter. This function must return a valid RivetPlugin object.
 const plugin: RivetPluginInitializer = (rivet) => {
-  const examplePlugin = {
+  // Initialize any nodes in here in the same way, by passing them the Rivet library.
+  const exampleNode = examplePluginNode(rivet);
+
+  // The plugin object is the definition for your plugin.
+  const examplePlugin: RivetPlugin = {
     // The ID of your plugin should be unique across all plugins.
-    id: "my rivet plugin",
+    id: "example-plugin",
+
     // The name of the plugin is what is displayed in the Rivet UI.
-    name: "my rivet plugin",
+    name: "Example Plugin",
+
     // Define all configuration settings in the configSpec object.
     configSpec: {
       exampleSetting: {
@@ -22,6 +28,7 @@ const plugin: RivetPluginInitializer = (rivet) => {
         helperText: "This is an example setting for the example plugin.",
       },
     },
+
     // Define any additional context menu groups your plugin adds here.
     contextMenuGroups: [
       {
@@ -30,14 +37,16 @@ const plugin: RivetPluginInitializer = (rivet) => {
       },
     ],
 
-    register: (register: <T extends ChartNode>(definition: PluginNodeDefinition<T>) => void) => {
-      // register(examplePluginNode(rivet))
-      // register(examplePluginNode2(rivet))
-      register(examplePluginNode3(rivet))
+    // Register any additional nodes your plugin adds here. This is passed a `register`
+    // function, which you can use to register your nodes.
+    register: (register) => {
+      register(exampleNode);
     },
   };
 
+  // Make sure to return your plugin definition.
   return examplePlugin;
 };
 
+// Make sure to default export your plugin.
 export default plugin;
--- src/nodes/ExamplePluginNode.ts
+++ src/nodes/ExamplePluginNode.ts
@@ -18,10 +18,13 @@ import type {
   PortId,
   Project,
   Rivet,
-} from '@ironclad/rivet-core';
+} from "@ironclad/rivet-core";
 
 // This defines your new type of node.
-export type ExamplePluginNode = ChartNode<'examplePlugin', ExamplePluginNodeData>;
+export type ExamplePluginNode = ChartNode<
+  "examplePlugin",
+  ExamplePluginNodeData
+>;
 
 // This defines the data that your new node will store.
 export type ExamplePluginNodeData = {
@@ -34,7 +37,7 @@ export type ExamplePluginNodeData = {
 
 // Make sure you export functions that take in the Rivet library, so that you do not
 // import the entire Rivet core library in your plugin.
-export function examplePluginNode3(rivet: typeof Rivet) {
+export function examplePluginNode(rivet: typeof Rivet) {
   // This is your main node implementation. It is an object that implements the PluginNodeImpl interface.
   const ExamplePluginNodeImpl: PluginNodeImpl<ExamplePluginNode> = {
     // This should create a new instance of your node type from scratch.
@@ -45,14 +48,14 @@ export function examplePluginNode3(rivet: typeof Rivet) {
 
         // This is the default data that your node will store
         data: {
-          someData: 'Hello World',
+          someData: "Hello World",
         },
 
         // This is the default title of your node.
-        title: 'Example Plugin Node',
+        title: "Example Plugin Node",
 
         // This must match the type of your node.
-        type: 'examplePlugin',
+        type: "examplePlugin",
 
         // X and Y should be set to 0. Width should be set to a reasonable number so there is no overflow.
         visualData: {
@@ -70,15 +73,15 @@ export function examplePluginNode3(rivet: typeof Rivet) {
       data: ExamplePluginNodeData,
       _connections: NodeConnection[],
       _nodes: Record<NodeId, ChartNode>,
-      _project: Project,
+      _project: Project
     ): NodeInputDefinition[] {
       const inputs: NodeInputDefinition[] = [];
 
       if (data.useSomeDataInput) {
         inputs.push({
-          id: 'someData' as PortId,
-          dataType: 'string',
-          title: 'Some Data',
+          id: "someData" as PortId,
+          dataType: "string",
+          title: "Some Data",
         });
       }
 
@@ -91,13 +94,13 @@ export function examplePluginNode3(rivet: typeof Rivet) {
       _data: ExamplePluginNodeData,
       _connections: NodeConnection[],
       _nodes: Record<NodeId, ChartNode>,
-      _project: Project,
+      _project: Project
     ): NodeOutputDefinition[] {
       return [
         {
-          id: 'someData' as PortId,
-          dataType: 'string',
-          title: 'Some Data',
+          id: "someData" as PortId,
+          dataType: "string",
+          title: "Some Data",
         },
       ];
     },
@@ -105,43 +108,56 @@ export function examplePluginNode3(rivet: typeof Rivet) {
     // This returns UI information for your node, such as how it appears in the context menu.
     getUIData(): NodeUIData {
       return {
-        contextMenuTitle: 'Example Plugin',
-        group: 'Example',
-        infoBoxBody: 'This is an example plugin node.',
-        infoBoxTitle: 'Example Plugin Node',
+        contextMenuTitle: "Example Plugin",
+        group: "Example",
+        infoBoxBody: "This is an example plugin node.",
+        infoBoxTitle: "Example Plugin Node",
       };
     },
 
     // This function defines all editors that appear when you edit your node.
-    getEditors(_data: ExamplePluginNodeData): EditorDefinition<ExamplePluginNode>[] {
+    getEditors(
+      _data: ExamplePluginNodeData
+    ): EditorDefinition<ExamplePluginNode>[] {
       return [
         {
-          type: 'string',
-          dataKey: 'someData',
-          useInputToggleDataKey: 'useSomeDataInput',
-          label: 'Some Data',
+          type: "string",
+          dataKey: "someData",
+          useInputToggleDataKey: "useSomeDataInput",
+          label: "Some Data",
         },
       ];
     },
 
     // This function returns the body of the node when it is rendered on the graph. You should show
     // what the current data of the node is in some way that is useful at a glance.
-    getBody(data: ExamplePluginNodeData): string | NodeBodySpec | NodeBodySpec[] | undefined {
+    getBody(
+      data: ExamplePluginNodeData
+    ): string | NodeBodySpec | NodeBodySpec[] | undefined {
       return rivet.dedent`
         Example Plugin Node
-        Data: ${data.useSomeDataInput ? '(Using Input)' : data.someData}
+        Data: ${data.useSomeDataInput ? "(Using Input)" : data.someData}
       `;
     },
 
     // This is the main processing function for your node. It can do whatever you like, but it must return
     // a valid Outputs object, which is a map of port IDs to DataValue objects. The return value of this function
     // must also correspond to the output definitions you defined in the getOutputDefinitions function.
-    async process(data: ExamplePluginNodeData, inputData: Inputs, _context: InternalProcessContext): Promise<Outputs> {
-      const someData = rivet.getInputOrData(data, inputData, 'someData', 'string');
+    async process(
+      data: ExamplePluginNodeData,
+      inputData: Inputs,
+      _context: InternalProcessContext
+    ): Promise<Outputs> {
+      const someData = rivet.getInputOrData(
+        data,
+        inputData,
+        "someData",
+        "string"
+      );
 
       return {
-        ['someData' as PortId]: {
-          type: 'string',
+        ["someData" as PortId]: {
+          type: "string",
           value: someData,
         },
       };
@@ -150,8 +166,11 @@ export function examplePluginNode3(rivet: typeof Rivet) {
 
   // Once a node is defined, you must pass it to rivet.pluginNodeDefinition, which will return a valid
   // PluginNodeDefinition object.
-  const examplePluginNode3 = rivet.pluginNodeDefinition(ExamplePluginNodeImpl, 'Example Plugin Node');
+  const examplePluginNode = rivet.pluginNodeDefinition(
+    ExamplePluginNodeImpl,
+    "Example Plugin Node"
+  );
 
   // This definition should then be used in the `register` function of your plugin definition.
-  return examplePluginNode3;
+  return examplePluginNode;
 }
