b79208a45ecb3b653b6a881b4151e82395318b46
Emeryck <emeryck.clerin@outlook.com>
2024-03-12 15:43:42 +0100

    try exemple 2

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,105 +1,3 @@
-// src/nodes/ExamplePluginNode.ts
-function examplePluginNode(rivet) {
-  const ExamplePluginNodeImpl = {
-    // This should create a new instance of your node type from scratch.
-    create() {
-      const node = {
-        // Use rivet.newId to generate new IDs for your nodes.
-        id: rivet.newId(),
-        // This is the default data that your node will store
-        data: {
-          someData: "Hello World"
-        },
-        // This is the default title of your node.
-        title: "Example Plugin Node",
-        // This must match the type of your node.
-        type: "examplePlugin",
-        // X and Y should be set to 0. Width should be set to a reasonable number so there is no overflow.
-        visualData: {
-          x: 0,
-          y: 0,
-          width: 200
-        }
-      };
-      return node;
-    },
-    // This function should return all input ports for your node, given its data, connections, all other nodes, and the project. The
-    // connection, nodes, and project are for advanced use-cases and can usually be ignored.
-    getInputDefinitions(data, _connections, _nodes, _project) {
-      const inputs = [];
-      if (data.useSomeDataInput) {
-        inputs.push({
-          id: "someData",
-          dataType: "string",
-          title: "Some Data"
-        });
-      }
-      return inputs;
-    },
-    // This function should return all output ports for your node, given its data, connections, all other nodes, and the project. The
-    // connection, nodes, and project are for advanced use-cases and can usually be ignored.
-    getOutputDefinitions(_data, _connections, _nodes, _project) {
-      return [
-        {
-          id: "someData",
-          dataType: "string",
-          title: "Some Data"
-        }
-      ];
-    },
-    // This returns UI information for your node, such as how it appears in the context menu.
-    getUIData() {
-      return {
-        contextMenuTitle: "Example Plugin",
-        group: "Example",
-        infoBoxBody: "This is an example plugin node.",
-        infoBoxTitle: "Example Plugin Node"
-      };
-    },
-    // This function defines all editors that appear when you edit your node.
-    getEditors(_data) {
-      return [
-        {
-          type: "string",
-          dataKey: "someData",
-          useInputToggleDataKey: "useSomeDataInput",
-          label: "Some Data"
-        }
-      ];
-    },
-    // This function returns the body of the node when it is rendered on the graph. You should show
-    // what the current data of the node is in some way that is useful at a glance.
-    getBody(data) {
-      return rivet.dedent`
-        Example Plugin Node
-        Data: ${data.useSomeDataInput ? "(Using Input)" : data.someData}
-      `;
-    },
-    // This is the main processing function for your node. It can do whatever you like, but it must return
-    // a valid Outputs object, which is a map of port IDs to DataValue objects. The return value of this function
-    // must also correspond to the output definitions you defined in the getOutputDefinitions function.
-    async process(data, inputData, _context) {
-      const someData = rivet.getInputOrData(
-        data,
-        inputData,
-        "someData",
-        "string"
-      );
-      return {
-        ["someData"]: {
-          type: "string",
-          value: someData
-        }
-      };
-    }
-  };
-  const examplePluginNode3 = rivet.pluginNodeDefinition(
-    ExamplePluginNodeImpl,
-    "Example Plugin Node"
-  );
-  return examplePluginNode3;
-}
-
 // src/nodes/ExamplePluginNode2.ts
 function examplePluginNode2(rivet) {
   const ExamplePluginNodeImpl = {
@@ -204,7 +102,6 @@ function examplePluginNode2(rivet) {
 
 // src/index.ts
 var plugin = (rivet) => {
-  const exampleNode = examplePluginNode(rivet);
   const exampleNode2 = examplePluginNode2(rivet);
   const examplePlugin = {
     // The ID of your plugin should be unique across all plugins.
@@ -230,7 +127,6 @@ var plugin = (rivet) => {
     // Register any additional nodes your plugin adds here. This is passed a `register`
     // function, which you can use to register your nodes.
     register: (register) => {
-      register(exampleNode);
       register(exampleNode2);
     }
   };
