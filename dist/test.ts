4516184ac0dc568faad1d4ac8c847c5b30b8fd7c
Emeryck <emeryck.clerin@outlook.com>
2024-03-12 10:14:41 +0100

    change back to the bundle exemple.js

--- dist/test.ts
+++
@@ -1,8 +0,0 @@
-import type { RivetPluginInitializer } from '@ironclad/rivet-core';
-
-const plugin: RivetPluginInitializer = (rivet) => ({
-  id: 'my-rivet-plugin',
-  name: 'my-rivet-plugin',
-});
-
-export default plugin;
